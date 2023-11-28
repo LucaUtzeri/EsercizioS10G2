63c27bc9dfcebc2ee20f16b36c240b6e21d5c97c
Luca Utzeri <lutzeri@libero.it>
2023-11-28 16:12:36 +0100

    initial commmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Route } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { PostAttiviComponent } from './pages/post-attivi/post-attivi.component';
+import { PostNonAttiviComponent } from './pages/post-non-attivi/post-non-attivi.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: NavbarComponent
+  },
+  {
+    path: 'post-attivi',
+    component: PostAttiviComponent
+  },
+  {
+    path: 'post-non-attivi',
+    component: PostNonAttiviComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostAttiviComponent,
+    PostNonAttiviComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/service/posts.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Post } from '../models/interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  constructor() { }
+}
+
